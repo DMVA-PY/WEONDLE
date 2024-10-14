bcfdca35149fd7ce57a6f74c8d39ae80bc4f7e87
Vare <dm.va@hotmail.com>
2024-10-14 02:44:16 -0300

    Fixed global.css bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/GILLIPODLE/",
+  base: "",
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
